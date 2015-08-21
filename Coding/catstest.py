c27e78eb92c1f851feb40f9073b4e298e7e21659
Melina <jab0003@jmss.vic.edu.au>
2015-08-21 10:56:35 +1000

    new file test 2 (do NOT run this code)

--- Coding/catstest.py
+++ Coding/catstest.py
@@ -3,3 +3,8 @@
 while True:
     print("cats")
 
+#state of files:
+#black means all good
+#blue means it's changed and uncommited: COMMIT ME
+#green means its a new file in git, but not yet committed
+#red means it hasn't been added to git: it doesnt exist according to git
